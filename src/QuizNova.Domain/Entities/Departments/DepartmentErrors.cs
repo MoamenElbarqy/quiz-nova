c6825aeb828358a574d5af0f3dd28b99af137136
Moamen Elbarky <momenatef530@gmail.com>
2026-04-15 01:51:08 +0200

    deleting unncessary models to keep foucs on the main functionality of the system

--- src/QuizNova.Domain/Entities/Departments/DepartmentErrors.cs
+++
@@ -1,9 +0,0 @@
-using QuizNova.Domain.Common.Results;
-
-namespace QuizNova.Domain.Entities.Departments;
-
-public static class DepartmentErrors
-{
-    public static readonly Error NameRequired =
-        Error.Validation("Department_Name_Required", "Department name is required.");
-}
