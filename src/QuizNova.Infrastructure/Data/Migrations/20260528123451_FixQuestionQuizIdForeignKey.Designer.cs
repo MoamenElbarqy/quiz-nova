8ec92f1843b466257a9c2432b52dc38970053a8b
Moamen Elbarky <momenatef530@gmail.com>
2026-05-28 17:20:41 +0300

    infra(db): configure explicit foreign key mapping and scaffold migration

--- src/QuizNova.Infrastructure/Data/Migrations/20260528123451_FixQuestionQuizIdForeignKey.Designer.cs
+++ src/QuizNova.Infrastructure/Data/Migrations/20260528123451_FixQuestionQuizIdForeignKey.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using QuizNova.Infrastructure.Data;
@@ -11,9 +12,11 @@ using QuizNova.Infrastructure.Data;
 namespace QuizNova.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260528123451_FixQuestionQuizIdForeignKey")]
+    partial class FixQuestionQuizIdForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
